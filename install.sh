09d26ea314e6548359c8245a83845c5cc07d0fa5
psych-d <dhrubasaha@outlook.com>
2022-06-09 22:03:11 +0530

    9/6/22

--- install.sh
+++ install.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 sudo add-apt-repository universe
 sudo apt-get update
-$echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial main universe" >>/etc/apt/sources.list
-$echo "deb-src http://us.archive.ubuntu.com/ubuntu/ xenial main universe" >>/etc/apt/sources.list
+sudo $echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial main universe" >>/etc/apt/sources.list
+sudo $echo "deb-src http://us.archive.ubuntu.com/ubuntu/ xenial main universe" >>/etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-2.0 guile-2.0-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev
-REQUIRED_PKG="unzip"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-    echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-    sudo apt-get --yes install $REQUIRED_PKG
-fi
+
+echo "select ubuntu version"
+echo " 1. 18.04 or newer"
+echo " 2. older thean 18.04"
+
+read option
+case $option in
+1)
+    echo "ubuntu 18.04 or newer"
+    sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-2.0 guile-2.0-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev
+    ;;
+2)
+    echo "older than ubuntu 18.04"
+    sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-1.8 guile-1.8-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev build-essential
+    ;;
+*) echo "invalid option" ;;
+esac
+
+wget https://github.com/psych-d/graphics.h/raw/main/archive/libgraph-1.0.2.tar.gz
+
+tar -xvf libgraph-1.0.2.tar.gz
+
+cd libgraph-1.0.2 && ./configure && make && sudo make install && sudo cp /usr/local/lib/libgraph.* /usr/lib && cd ..
+rm -r libgraph-1.0.2 
+rm libgraph-1.0.2.tar.gz
+
+echo "Successfully installed !"
