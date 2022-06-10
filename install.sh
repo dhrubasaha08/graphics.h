c38648aa1094d0362386e48e0612d189fa5e54fa
dhrubasaha08 <dhrubasaha@outlook.com>
2022-06-10 13:21:39 +0530

    initial commit

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo add-apt-repository universe -y
-sudo apt install gcc -y
+sudo apt install gcc g++ make make-guile -y
+sudo apt-get install libsdl-image1.2 libsdl1.2-dev libsdl-image1.2-dev libaudiofile-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev build-essential libsdl1.2debian libart-2.0-dev -y
 
 echo "select ubuntu version"
 echo " 1. 18.04 or newer"
@@ -13,11 +14,11 @@ read option
 case $option in
 1)
     echo "ubuntu 18.04 or newer"
-    sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-2.0 guile-2.0-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev
+    sudo apt install guile-2.0 guile-2.0-dev -y
     ;;
 2)
     echo "older than ubuntu 18.04"
-    sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-1.8 guile-1.8-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev build-essential
+    sudo apt install guile-1.8 guile-1.8-dev -y
     ;;
 *) echo "invalid option" ;;
 esac
