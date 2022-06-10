e5644fb10c9feeb9169e020c50aadfc09b7fcecb
psych-d <dhrubasaha@outlook.com>
2022-06-10 18:52:21 +0530

    initial

--- install.sh
+++ install.sh
@@ -2,26 +2,45 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo add-apt-repository universe -y
 sudo apt install gcc g++ make make-guile -y
 sudo apt-get install libsdl-image1.2 libsdl1.2-dev libsdl-image1.2-dev libaudiofile-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev libslang2-dev libasound2 libasound2-dev build-essential libsdl1.2debian libart-2.0-dev -y
 
-echo "select ubuntu version"
-echo " 1. 18.04 or newer"
-echo " 2. older thean 18.04"
+###     os selection menu   ###
+
+echo "Select OS Version"
+
+#echo "<serial>. <distro_name> <code_name> XX.XX.XX"
+
+echo "1. Ubuntu Xenial 16.04.XX"
+echo "2. Ubuntu Bionic 18.04.XX"
+echo "3. Ubuntu Focal 20.04.XX"
+echo "4. Ubuntu Jammy 22.04.XX"
 
 read option
 case $option in
+
 1)
-    echo "ubuntu 18.04 or newer"
-    sudo apt install guile-2.0 guile-2.0-dev -y
+    echo "Ubuntu Xenial 16.04.XX"
+    sudo add-apt-repository universe -y
+    sudo apt install guile-1.8 guile-1.8-dev -y
     ;;
 2)
-    echo "older than ubuntu 18.04"
+    echo "Ubuntu Bionic 18.04.XX"
+    sudo apt install guile-2.0 guile-2.0-dev -y
+    ;;
+3)
+    echo "Ubuntu Focal 20.04.XX"
+    sudo apt install guile-1.8 guile-1.8-dev -y
+    ;;
+
+4)
+    echo "Ubuntu Jammy 22.04.XX"
     sudo apt install guile-1.8 guile-1.8-dev -y
     ;;
 *) echo "invalid option" ;;
 esac
+
+###     end of os selection menu    ###
 
 CPPFLAGS="$CPPFLAGS $(pkg-config --cflags-only-I guile-2.0)" CFLAGS="$CFLAGS $(pkg-config --cflags-only-other guile-2.0)" LDFLAGS="$LDFLAGS $(pkg-config --libs guile-2.0)"
 
