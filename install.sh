cd5634c91aab22a9a017ff5b2d2a841f46abc0e4
Dhruba Saha <dhrubasaha@outlook.com>
2022-11-12 11:06:40 +0530

    shebang position fixed!

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Auto install graphics.h in Ubuntu using bash script
 # Copyright (C) 2022  Dhruba Saha
 #
@@ -13,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#!/bin/bash
 
 
 #installing dependencies
